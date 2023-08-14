685a78bf006007ba0f1569e804ec1d21d30ce607
Jubayer05 <jubayer0504@gmail.com>
2023-08-14 17:17:02 +0600

    working on team section

--- components/About/Team.js
+++ components/About/Team.js
@@ -1,27 +1,57 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 const Team = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const openModal = (item) => {
+    setModalData(item);
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const customStyles = {
+    content: {
+      background: '#fff',
+      // innerWidth: '768px',
+      top: '55%',
+      left: '50%',
+      right: 'auto',
+      bottom: '-30%',
+      // marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: 100,
+    },
+  };
+
   const teamMembers = [
     {
       name: 'Nazmus Sakib',
       profession: 'Founder and CEO at Data Solution 360',
       img: '/team/sakib.jpg',
+      details: '',
     },
     {
       name: 'Shaikh Farhad Hossain, PhD',
       profession: 'Trainer and Advisor Data Scientist and AI Researcher',
       img: '/team/Farhad.jpg',
+      details: '',
     },
     {
       name: 'Noor Ahmed Sarker',
       profession: 'CTO at Data Solution 360',
       img: '/team/noor.jpeg',
+      details: `<p>At the moment, Mr. Noor Ahmad Sarker serves as Data Solution 360's CTO. In 2021, he graduated with a Bachelor of Science (B.Sc.) from Rajshahi University of Engineering & Technology (RUET). Later, in October 2022, he started working for Data Solution 360 as an analytics engineer before being promoted to CTO in March 2023. <br/> <br/> Mr. Noor enjoys learning about and researching data science. He is currently working on some projects related to Data Analytics & Machine Learning after successfully completing several projects on data analysis. His ambition is to work as a researcher in the field of data science. <br/><br/> Mr. Noor is very knowledgeable about various data analytics and visualization tools. He utilized well-known Python libraries like Numpy, Pandas, and others to work on a variety of data analysis and machine learning projects.</p>`,
     },
     {
       name: 'Md. Monjur',
       profession: 'Business development manager at Data Solution 360',
       img: '/team/monjur.jpeg',
+      details: `<p>Md. Monjur is working as business development manager at Data Solution 360. He has completed his BBA from the university of Dhaka recently and he was the president of Dhaka University Entrepreneurship Development Club and Bizniverse Today. <br/><br/> He loves to learn and explore business. He reads marketing and branding related books a lot. Apert from that, he has a great knack on literature.</p>`,
     },
   ];
 
@@ -48,6 +78,55 @@ const Team = () => {
                   {item.name}
                 </p>
                 <p className="text-sm text-gray-600 -mt-3">{item.profession}</p>
+
+                <>
+                  <a type="button" onClick={() => openModal(item)}>
+                    view details
+                  </a>
+                  <Modal
+                    isOpen={modalIsOpen}
+                    // onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                  >
+                    <div className="max-w-xl mx-5 z-30">
+                      <div className="flex items-center justify-center mb-[15px] text-center">
+                        <img
+                          className="mx-auto w-[300px] h-[300px] object-cover rounded-[3px]"
+                          src={modalData?.img}
+                          alt={modalData?.name}
+                        />
+                      </div>
+                      <div>
+                        <h2 className="text-xl font-bold leading-8 text-[#231f40]">
+                          {modalData?.name}
+                        </h2>
+                        <p className="text-[#525fe1] font-medium -mt-2">
+                          {modalData?.profession}
+                        </p>
+                        <div
+                          className="text-sm text-gray-600 mt-5"
+                          dangerouslySetInnerHTML={{
+                            __html: modalData?.details,
+                          }}
+                        />
+                      </div>
+                      <p className="text-base leading-7 text-[#6f6b80] mb-[24px]">
+                        {modalData?.reviewDetails}
+                      </p>
+
+                      <div className="text-center pt-6">
+                        <button
+                          className="bg-[#ff3f3f] text-white rounded-lg px-5 py-2"
+                          onClick={closeModal}
+                        >
+                          close
+                        </button>
+                      </div>
+                    </div>
+                  </Modal>
+                </>
               </div>
             </div>
           ))}
