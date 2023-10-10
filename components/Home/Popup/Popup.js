ada1ca4f0b27fe666cda5a511ecd33b18e1272e3
Jubayer05 <jubayer0504@gmail.com>
2023-10-11 00:39:28 +0600

    popup update

--- components/Home/Popup/Popup.js
+++ components/Home/Popup/Popup.js
@@ -6,8 +6,11 @@ const Popup = ({ handler }) => {
   return (
     <div className="w-screen h-screen fixed top-0 left-0 bg-[#0000008f] z-30 flex justify-center items-center">
       <div
-        style={{ backgroundImage: 'url("/course/photo_popup-01.jpeg")' }}
-        className=" h-[400px] md:h-[450px] lg:h-[580px] w-[350px] md:w-[480px] lg:w-[600px] p-2 rounded-md relative flex justify-center items-end bg-cover"
+        style={{
+          backgroundImage:
+            'url("https://firebasestorage.googleapis.com/v0/b/data-solution-360.appspot.com/o/courseImage%2Fjubayer0504%40gmail.com%2Ffinal-fianl%20(1).jpg?alt=media&token=d3cec0eb-e10d-4319-9d51-0f99d0289b28")',
+        }}
+        className=" h-[230px] md:h-[380px] lg:h-[480px] w-[350px] md:w-[600px] lg:w-[800px] p-2 rounded-md relative flex justify-center items-end bg-cover"
       >
         {/* <div>
           <h2 className="font-heading text-2xl md:text-4xl text-center mt-4">
@@ -25,7 +28,7 @@ const Popup = ({ handler }) => {
           </div>
         </div> */}
         {/* <div>&nbsp;</div> */}
-        <Link href="course-details/YjLhG3VxLBwv4w8DsAPb">
+        <Link href="course-details/g0fAq19lGtOen9OmXp69">
           <button
             className={`text-md px-3 py-2 hover:drop-shadow-xl flex items-center text-gray-300 
           bg-primary-bg transition-all duration-300 ease-linear `}
