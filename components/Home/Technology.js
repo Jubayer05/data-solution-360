cfc98e1eb1a48cbda51d63016bd6a0718b15673b
Jubayer05 <jubayer0504@gmail.com>
2024-06-09 02:25:18 +0600

    add customer chat

--- components/Home/Technology.js
+++ components/Home/Technology.js
@@ -9,20 +9,23 @@ const Technology = () => {
     <div className="bg-[#f9f9fa]	py-4 md:pt-20 px-3" id="courses">
       <div
         style={{ backgroundImage: "url('/Background/bg-2.jpg')" }}
-        className="max-w-6xl relative mx-auto rounded-lg shadow bg-cover bg-center overflow-hidden"
+        className="max-w-6xl relative mx-auto rounded-lg shadow bg-cover bg-center overflow-hidden
+        "
       >
-        <div className="w-full h-full bg-black opacity-70 absolute" />
-        <div className="flex">
+        {/* <div className="w-full h-full bg-black opacity-70 absolute" /> */}
+        <div className="bg-[rgba(0,0,0,0.51)]">
           <div className="py-4 md:py-8 z-50">
             <h2 className="text-center text-3xl font-bold font-heading mt-4 text-white">
               Technology Stack
             </h2>
             <div className="w-36 h-1.5 bg-gradient-to-r from-orange-600 to-blue-700 rounded-full mx-auto"></div>
-            <p className="text-center mt-6 text-sm md:text-xl sm:w-2/3 mx-auto px-4 text-white">
-              The field of data science has evolved to a stage where no
-              organization can ignore it while setting up their data science
-              tech stack.
-            </p>
+            <div className="mt-6 text-sm md:text-xl sm:w-2/3 mx-auto text-justify md:text-center  px-4 text-white">
+              <p>
+                The field of data science has evolved to a stage where no
+                organization can ignore it while setting up their data science
+                tech stack.
+              </p>
+            </div>
 
             <div className="mt-8 max-w-5xl mx-auto">
               <Marquee speed={30} className="pb-8 pt-4">
--- components/utilities/WhatsApp.js
+++ components/utilities/WhatsApp.js
@@ -1,26 +1,86 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import { FacebookProvider, CustomChat } from 'react-facebook';
+import Link from 'next/link';
+import React, { useEffect, useRef, useState } from 'react';
+import { FiPhoneCall } from 'react-icons/fi';
+import { IoLogoWhatsapp } from 'react-icons/io5';
 
 const WhatsApp = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const popupRef = useRef(null);
+
   const handleClick = () => {
     const phoneNumber = '8801996104096'; // Replace with your desired phone number
     const url = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
     window.open(url, '_blank');
   };
 
+  const handleOpenPopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (popupRef.current && !popupRef.current.contains(event.target)) {
+      setShowPopup(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showPopup) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    }
+  }, [showPopup]);
+
   return (
     <div>
       <div
-        className="w-[60px] fixed bottom-[6rem] md:bottom-24 right-[1.55rem] md:right-7 cursor-pointer"
-        onClick={handleClick}
+        ref={popupRef}
+        className={`fixed flex items-center flex-col gap-2 bottom-[20px] md:bottom-24  
+          bg-white px-4 py-3 rounded-xl font-bangla text-lg w-72 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] 
+          z-50 transition-all duration-500 ${
+            showPopup
+              ? 'opacity-100 visible scale-100 right-7'
+              : 'opacity-0 invisible scale-[0.3] -right-20 md:-bottom-[30px]'
+          }`}
       >
-        <img src="/whatsapp.png" alt="whatsapp" />
+        <p className="text-xl font-bold text-center">
+          কথা বলুন <br /> ক্যারিয়ার কাউন্সিলরের সাথে
+        </p>
+        <img
+          src="/course/support.png"
+          alt="whatsapp"
+          className="w-[150px] h-[150px] rounded-full"
+        />
+        <div className="h-[50px]" />
+        <button
+          className="bg-[#eaecf0] hover:bg-[#d2d3d5] transition-all duration-300 
+        w-full py-2 rounded-md "
+        >
+          <Link
+            href="tel:+8801996104096"
+            className="flex items-center justify-center gap-2 "
+          >
+            <FiPhoneCall className="text-xl" />
+            <span>Call</span>
+          </Link>
+        </button>
+        <button
+          onClick={handleClick}
+          className="flex items-center justify-center bg-[#12b76a] hover:bg-[rgb(10,145,82)] 
+          transition-all duration-300 w-full gap-2 py-2 rounded-md text-white"
+        >
+          <IoLogoWhatsapp className="text-2xl" />
+          <span>Whatsapp</span>
+        </button>
       </div>
 
-      <FacebookProvider appId="719749473297599" chatSupport>
-        <CustomChat  pageId="131657960804162" minimized={false} />
-      </FacebookProvider>
+      <div
+        className="fixed flex items-center gap-2 bottom-[20px]] md:bottom-8 right-[1.55rem] md:right-7
+         cursor-pointer bg-slate-700 text-white px-4 py-3 rounded-xl font-bangla text-lg"
+        onClick={handleOpenPopup}
+      >
+        <FiPhoneCall />
+        <span>কল করুন</span>
+      </div>
     </div>
   );
 };
